56c183477d2b8a8f6ea0be014f47c682e1165418
nemesis2021 <96518328+nemesis2021@users.noreply.github.com>
2023-12-15 13:45:36 +0000

    Update service_status.bash

--- service_status.bash
+++ service_status.bash
@@ -5,9 +5,9 @@ MARIA_STAT=$(systemctl status mariadb | grep -I "Active" | awk {'print $02, $03,
 PHP_STAT=$(PHP_VERSION=$(php -v | tac | tail -n 1 | cut -d " " -f 2 | cut -c 1-3) && systemctl status php$PHP_VERSION-fpm | grep -I "Active" | awk {'print $02, $03, $04, $05, $06, $07, $08, $09, $10, $11'});
 echo "Nginx: $NGINX_STAT";
 echo "MariaDB: $MARIA_STAT";
-echo "PHP: $PHP_STAT";
+echo -e "PHP: $PHP_STAT\\n";
 
-echo -e "=================\\n";
-echo -e "WordPress Detail\\n";
 echo -e "=================";
+echo -e "WordPress Details";
+echo -e "=================\\n";
  
