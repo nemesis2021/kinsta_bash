44326e4257b0602f9ec61924e7e2459cbcc27b6b
nemesis2021 <96518328+nemesis2021@users.noreply.github.com>
2023-12-15 13:58:21 +0000

    path

--- service_status.bash
+++ service_status.bash
@@ -3,7 +3,7 @@
 NGINX_STAT=$(systemctl status nginx | grep -I "Active" | awk {'print $02, $03, $04, $05, $06, $07, $08, $09, $10, $11'});
 MARIA_STAT=$(systemctl status mariadb | grep -I "Active" | awk {'print $02, $03, $04, $05, $06, $07, $08, $09, $10, $11'});
 PHP_STAT=$(PHP_VERSION=$(php -v | tac | tail -n 1 | cut -d " " -f 2 | cut -c 1-3) && systemctl status php$PHP_VERSION-fpm | grep -I "Active" | awk {'print $02, $03, $04, $05, $06, $07, $08, $09, $10, $11'});
-echo "Nginx: $NGINX_STAT";
+echo -e "\\nNginx: $NGINX_STAT";
 echo "MariaDB: $MARIA_STAT";
 echo -e "PHP: $PHP_STAT\\n";
 
@@ -14,10 +14,9 @@ echo -e "=================\\n";
 SITEURL=$(wp option get siteurl);
 HOMEURL=$(wp option get home);
 WPLOGIN=$(wp eval 'echo wp_login_url() . "\n";');
+WPPATH=$(wp eval 'echo wp_login_url() . "\n";');
 
  echo "Site URL: $SITEURL";
- echo "Home URL: $HOMEURL";
+ echo -e "Home URL: $HOMEURL\\n";
  echo "WP Login: $WPLOGIN";
-
-
-
+ echo -e "WP PATH: $WPPATH";
