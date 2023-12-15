ab5fd3179651ec991fe1cbe8947f66952a77497c
nemesis2021 <96518328+nemesis2021@users.noreply.github.com>
2023-12-15 15:03:18 +0000

    fix error

--- service_status.bash
+++ service_status.bash
@@ -8,7 +8,7 @@ NGINX_STAT=$(systemctl status nginx | grep -I "Active" | awk {'print $02, $03, $
 MARIA_STAT=$(systemctl status mariadb | grep -I "Active" | awk {'print $02, $03, $04, $05, $06, $07, $08, $09, $10, $11'});
 PHP_STAT=$(PHP_VERSION=$(php -v | tac | tail -n 1 | cut -d " " -f 2 | cut -c 1-3) && systemctl status php$PHP_VERSION-fpm | grep -I "Active" | awk {'print $02, $03, $04, $05, $06, $07, $08, $09, $10, $11'});
 
-clearecho -e "\\nNginx: $NGINX_STAT";
+echo -e "\\nNginx: $NGINX_STAT";
 echo "MariaDB: $MARIA_STAT";
 echo -e "PHP: $PHP_STAT\\n";
 
