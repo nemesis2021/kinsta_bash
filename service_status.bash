65e0600d188bd936376fd9765dad5ebb30845786
nemesis2021 <96518328+nemesis2021@users.noreply.github.com>
2023-12-15 14:14:26 +0000

    services

--- service_status.bash
+++ service_status.bash
@@ -1,9 +1,14 @@
 
 #!/bin/bash
+
+echo -e "\\n=================";
+echo -e "Services";
+echo -e "=================";
 NGINX_STAT=$(systemctl status nginx | grep -I "Active" | awk {'print $02, $03, $04, $05, $06, $07, $08, $09, $10, $11'});
 MARIA_STAT=$(systemctl status mariadb | grep -I "Active" | awk {'print $02, $03, $04, $05, $06, $07, $08, $09, $10, $11'});
 PHP_STAT=$(PHP_VERSION=$(php -v | tac | tail -n 1 | cut -d " " -f 2 | cut -c 1-3) && systemctl status php$PHP_VERSION-fpm | grep -I "Active" | awk {'print $02, $03, $04, $05, $06, $07, $08, $09, $10, $11'});
-echo -e "\\nNginx: $NGINX_STAT";
+
+clearecho -e "\\nNginx: $NGINX_STAT";
 echo "MariaDB: $MARIA_STAT";
 echo -e "PHP: $PHP_STAT\\n";
 
@@ -20,3 +25,5 @@ WPPATH=$(wp eval 'echo get_home_path();');
  echo -e "Home URL: $HOMEURL\\n";
  echo "WP Login: $WPLOGIN";
  echo -e "WP PATH: $WPPATH";
+
+
