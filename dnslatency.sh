f1a7325c3177db4ab7220c932c600b161bdd2bf9
Adrian <creed2011nemesis@gmail.com>
2024-11-03 14:44:47 +0000

    add color

--- dnslatency.sh
+++ dnslatency.sh
@@ -3,11 +3,12 @@ curl -s -o /dev/null -w "facebook.com Total: %{time_total}s\n" https://facebook.
 curl -s -o /dev/null -w "msn.com Total: %{time_total}s\n" https://msn.com
 
 echo "--------------------"
-echo Imagick Version:
+echo -e "\033[31mImagick Version:\033[0m"
 grep mageMagickVersion /kinsta/main.conf && dpkg -l |grep imageMagickVersion=
 echo "--------------------"
 
 echo "--------------------"
+echo -e "\033[31mDatabase:\033[0m"
 echo Database:
 echo "--------------------"
 curl -s https://raw.githubusercontent.com/major/MySQLTuner-perl/master/mysqltuner.pl | perl | awk '/General recommendations:/,/^$/'
