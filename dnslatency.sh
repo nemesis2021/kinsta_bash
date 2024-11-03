5ced7089bdc58e3100f9f1edc5941c16ca96583a
Adrian <creed2011nemesis@gmail.com>
2024-11-03 14:42:13 +0000

    add perl

--- dnslatency.sh
+++ dnslatency.sh
@@ -6,3 +6,8 @@ echo "--------------------"
 echo Imagick Version:
 grep mageMagickVersion /kinsta/main.conf && dpkg -l |grep imageMagickVersion=
 echo "--------------------"
+
+echo "--------------------"
+echo Database:
+echo "--------------------"
+curl -s https://raw.githubusercontent.com/major/MySQLTuner-perl/master/mysqltuner.pl | perl | awk '/General recommendations:/,/^$/'
