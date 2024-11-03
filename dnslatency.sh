f257fa9317911d80aeed94515e1f1eb723354fc7
Adrian <creed2011nemesis@gmail.com>
2024-11-03 14:48:45 +0000

    Color

--- dnslatency.sh
+++ dnslatency.sh
@@ -1,3 +1,7 @@
+
+echo "--------------------"
+echo -e "\033[31mDNS Latency:\033[0m"
+echo "--------------------"
 curl -s -o /dev/null -w "google.com Total: %{time_total}s\n" https://google.com
 curl -s -o /dev/null -w "facebook.com Total: %{time_total}s\n" https://facebook.com
 curl -s -o /dev/null -w "msn.com Total: %{time_total}s\n" https://msn.com
@@ -9,6 +13,5 @@ echo "--------------------"
 
 echo "--------------------"
 echo -e "\033[31mDatabase:\033[0m"
-echo Database:
 echo "--------------------"
 curl -s https://raw.githubusercontent.com/major/MySQLTuner-perl/master/mysqltuner.pl | perl | awk '/General recommendations:/,/^$/'
