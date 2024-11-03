70f62347461921ec4ece892136da64d8cf2f6299
Adrian <creed2011nemesis@gmail.com>
2024-11-03 14:34:38 +0000

    add imagick

--- dnslatency.sh
+++ dnslatency.sh
@@ -1,3 +1,6 @@
 curl -s -o /dev/null -w "google.com Total: %{time_total}s\n" https://google.com
 curl -s -o /dev/null -w "facebook.com Total: %{time_total}s\n" https://facebook.com
 curl -s -o /dev/null -w "msn.com Total: %{time_total}s\n" https://msn.com
+
+echo Imagick Version:
+grep mageMagickVersion /kinsta/main.conf && dpkg -l |grep imageMagickVersion=
